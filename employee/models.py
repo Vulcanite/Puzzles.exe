5fcf9018b69f86a9800e1d2ada0dae25395d78a3
Tanmay <tanmay44200@gmail.com>
2022-08-25 17:44:22 +0530

    Support Ticket System Update

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+TAGS = (
+    ('Self repair', 'Self Repair'),
+    ('Request Technician', 'Request Technician')
+)
+
+class RequestTable(models.Model):
+    title = models.CharField(max_length=150)
+    request_tags = models.CharField(choices=TAGS, max_length=50, default='Request Technician')
+    description = models.CharField(max_length=250)
+    
--- employee/views.py
+++ employee/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
+from employee.models import RequestTable
+
 def dashboard(request):
     return render(request, "employee/dashboard.html")
 
+def supportRequest(request):
+    return render(request, "employee/support-form.html")
+
 def addRequest(request):
-    pass
+    if request.method == 'POST':
+        save_request = RequestTable(title = request.POST.get('titleInput'),
+                                request_tags = request.POST.get('inputTag'),
+                                description = request.POST.get('descInput'))
+        save_request.save()
+    return render(request, "employee/support-form.html")
 
 def fetchConfiguration(request):
     pass
