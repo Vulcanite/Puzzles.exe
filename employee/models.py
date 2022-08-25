f6fad50c6f1ecd1884d9c635dd83f71437ddb0c8
Vulcanite <amishtp147@outlook.com>
2022-08-26 00:23:55 +0530

    Updated Support Request Table

--- employee/models.py
+++ employee/models.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from django.db import models
+from UserAuth.models import customuser
 
 # Create your models here.
 TAGS = (
@@ -12,6 +13,7 @@ STATUS_LIST = (
     ('APPROVED', 'APPROVED'),
     ('REJECTED', 'REJECTED')
 )
+
 status_list = ["OPENED", "APPROVED", "REJECTED"]
 
 class RequestTable(models.Model):
@@ -20,3 +22,4 @@ class RequestTable(models.Model):
     description = models.CharField(max_length=250)
     last_update = models.DateTimeField(default=dt.datetime.utcnow() + dt.timedelta(hours=5, minutes=30), blank=True)
     status =  models.CharField(choices=STATUS_LIST, max_length=50, default='OPENED')
+    created_by = models.ForeignKey(customuser, on_delete=models.CASCADE, blank = True, null = True)
--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from django.shortcuts import render
 
 from employee.models import RequestTable
@@ -12,8 +13,8 @@ def addRequest(request):
     if request.method == 'POST':
         save_request = RequestTable(title = request.POST.get('titleInput'),
                                 request_tags = request.POST.get('inputTag'),
-                                description = request.POST.get('descInput'))
-        print(save_request)
+                                description = request.POST.get('descInput'),
+                                created_by = request.user)
         save_request.save()
     return render(request, "employee/support-form.html")
 
