a632712e662665ff76aef46e9008f8e0a1e2cddf
salonidhotre <salonidhotre17.a@gmail.com>
2022-08-26 06:47:31 +0530

    UI Updated

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("members/", views.memberPage, name="helpdesk/members"),
     path("tickets/", views.getSupportTickets, name="helpdesk/tickets"),
     path("ticket/<int:ticketId>/", views.getTicketDetails, name="helpdesk/ticket"),
-    path("ajax/validate_username/",views.hardware_details)
+    path("ajax/validate_username/",views.hardware_details),
+    path('tech-logs/', views.getTechnicalLogs, name="technical-logs")
 ]
