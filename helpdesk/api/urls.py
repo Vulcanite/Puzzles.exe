0416886fb99ab68b27141d8eff4cdfe74b2c8d89
Vulcanite <amishtp147@outlook.com>
2022-08-26 15:27:28 +0530

    Code Update

--- helpdesk/api/urls.py
+++ helpdesk/api/urls.py
@@ -2,6 +2,6 @@ from .views import save_hardware_to_db
 from django.urls import path
 
 urlpatterns = [
-    path('getData/', save_hardware_to_db, name="getData"), #POST
+    path('getData/', save_hardware_to_db, name="getData")
 ]
 
--- helpdesk/models.py
+++ helpdesk/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class hardware_details(models.Model):
     emp_pc_config = models.JSONField()
-    emp_ip_address = models.CharField(max_length=100)
+    emp_ip_address = models.CharField(max_length=100, unique=True)
